ed79662253f8ae4f0b95bd21414ed3229dcf8939
aleksei_fedorov@epam.com <aleksei_fedorov@epam.com>
2021-10-18 01:16:54 +0300

    Fixed Jersey config

--- src/main/java/my/assignment/configuration/JerseyConfig.java
+++ src/main/java/my/assignment/configuration/JerseyConfig.java
@@ -4,24 +4,23 @@ import io.swagger.jaxrs.config.BeanConfig;
 import io.swagger.jaxrs.listing.ApiListingResource;
 import io.swagger.jaxrs.listing.SwaggerSerializers;
 import my.assignment.controller.DictionaryController;
+import my.assignment.controller.DictionaryExceptionHandler;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-
 @Component
+@Configuration
 public class JerseyConfig extends ResourceConfig {
 
-    @PostConstruct
-    public void init() {
-        this.configureSwagger();
-    }
+    JerseyConfig() {
+        BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setResourcePackage("my.assignment");
 
-    private void configureSwagger() {
-        this.packages("my.assignment");
         this.register(ApiListingResource.class);
         this.register(SwaggerSerializers.class);
         this.register(DictionaryController.class);
+        this.register(DictionaryExceptionHandler.class);
 
         BeanConfig config = new BeanConfig();
         config.setSchemes(new String[]{"http", "https"});
